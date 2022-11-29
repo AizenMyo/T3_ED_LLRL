1bce552d4ee29fa65b1eb7e2f119964fb0e437d0
Luis <luiki.lrl@gmail.com>
2022-11-29 19:46:31 +0100

    HolaMundoZ

--- Pro_LLRL/src/HolaMundo.java
+++ Pro_LLRL/src/HolaMundo.java
@@ -1,9 +1,6 @@
 
 public class HolaMundo {
-
-	public static void main(String[] args) {
-		System.out.println("Hola mundo soy Luis Lopez De Rueda Laguna");
-
-	}
-
+public static void main(String[] args) {
+	System.out.println("Hola Mundo soy Luis Lopez de Reuda Laguna");
+}
 }
